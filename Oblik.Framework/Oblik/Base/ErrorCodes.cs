48f616896855d31d7a757d84435788cbf3ec6411
Samael7777 <vadim.kutyn@gmail.com>
2021-12-16 13:17:30 +0200

    20211216 debug ver

--- Oblik.Framework/Oblik/Base/ErrorCodes.cs
+++ Oblik.Framework/Oblik/Base/ErrorCodes.cs
@@ -29,34 +29,4 @@
         NotReadableSegError = 21,
         NotEraseableSegError = 22,
     }
-
-    /// <summary>
-    /// Уровень доступа
-    /// 0 - пользователь;
-    /// 1 - администратор;
-    /// 2 - энергонадзор;
-    /// 3 - служебный пользователь.
-    /// </summary>
-    public enum UserLevel
-    {
-        /// <summary>
-        /// Пользователь
-        /// </summary>
-        User = 0,
-
-        /// <summary>
-        /// Администратор
-        /// </summary>
-        Admin = 1,
-
-        /// <summary>
-        /// Энергонадзор
-        /// </summary>
-        Energo = 2,
-
-        /// <summary>
-        /// Системный пользователь
-        /// </summary>
-        System = 3
-    }
 }
---
+++ Oblik.Framework/Oblik/Base/UserLevel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oblik
+{ 
+    /// <summary>
+    /// Уровень доступа
+    /// 0 - пользователь;
+    /// 1 - администратор;
+    /// 2 - энергонадзор;
+    /// 3 - служебный пользователь.
+    /// </summary>
+    public enum UserLevel
+    {
+        /// <summary>
+        /// Пользователь
+        /// </summary>
+        User = 0,
+
+        /// <summary>
+        /// Администратор
+        /// </summary>
+        Admin = 1,
+
+        /// <summary>
+        /// Энергонадзор
+        /// </summary>
+        Energo = 2,
+
+        /// <summary>
+        /// Системный пользователь
+        /// </summary>
+        System = 3
+    }
+}
